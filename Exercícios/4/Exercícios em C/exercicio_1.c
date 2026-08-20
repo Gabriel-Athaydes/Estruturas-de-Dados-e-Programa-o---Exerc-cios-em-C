ef779fe6f49024a112742e03053c477f4a4e5a37
Gabriel-Athaydes <123844270+Gabriel-Athaydes@users.noreply.github.com>
2026-08-20 16:54:57 -0300

    Update exercicio_1.c

--- Exercícios/4/Exercícios em C/exercicio_1.c
+++ Exercícios/4/Exercícios em C/exercicio_1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    char *a = "Aula de estruturas de dados e programação";
+    char a[] = "Aula de estruturas de dados e programação";
     int processo = -1;
     
     printf("%s\n",a);
     
-    for (int i=0;i<43-4;i++) {
+    for (int i=0;i<strlen(a)-4;i++) {
         if ('s'==a[i]&&'t'==a[i+1]&&'r'==a[i+2]&&'u'==a[i+3]&&'t'==a[i+4]){
             processo=i;
         }
