b68278c0cefba81cac10380e2371d9026c8fa7dd
Gabriel-Athaydes <123844270+Gabriel-Athaydes@users.noreply.github.com>
2026-08-27 16:38:23 -0300

    Update exercicio_1.c

--- Exercícios/5/Exercícios em C/exercicio_1.c
+++ Exercícios/5/Exercícios em C/exercicio_1.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <string.h>
 
+int inversor(int vet[3][3])
+{
+    int maior = vet[0][0];
+    int quant = 1;
+    for (int i=0;i<3;i++)
+        for (int n=0;n<3;n++){
+            printf("%d ",vet[n][i]);
+            if (vet[n][i]>maior)
+                maior=vet[n][i];
+        }
+    while (maior>0){
+        quant++;
+        maior/=10;
+    }
+    return quant;
+}
+
+int main()
+{
+    int vet[3][3] = {{1, 2, 3,}, {4, 15, 6,}, {7, 8, 439,}};
+    printf("\n%d", inversor(vet));
+
+    return 0;
+}
